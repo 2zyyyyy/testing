725ec0ad9ce380960ee9a07b2f7449ed7a40de1c
gilbert <2zyyyyy4dev@gmail.com>
2023-03-20 21:57:18 +0800

    add:文件操作

--- coding/test.py
+++ coding/test.py
@@ -1,10 +1,20 @@
 
-class Dog:
-    def __init__(self, name):
-        self.name = name
+# class Dog:
+#     def __init__(self, name):
+#         self.name = name
 
 
-d = Dog('wangcai')
-# hasattr：判断对象是否有该属性
-print(hasattr(d, 'name'))
-print(hasattr(d, 'age'))
+# d = Dog('wangcai')
+# # hasattr：判断对象是否有该属性
+# print(hasattr(d, 'name'))
+# print(hasattr(d, 'age'))
+
+# locals
+def test():
+    print('before define a')
+    print(locals())
+    a = 1
+    print('after define a')
+    print(locals())
+
+test()
