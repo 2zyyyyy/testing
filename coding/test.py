5bda0cd0d7ba27cf17fae2534025869602c4b00d
gilbert <2zyyyyy4dev@gmail.com>
2023-03-22 23:11:52 +0800

    add:性能测试相关

--- coding/test.py
+++ coding/test.py
@@ -10,11 +10,36 @@
 # print(hasattr(d, 'age'))
 
 # locals
-def test():
-    print('before define a')
-    print(locals())
-    a = 1
-    print('after define a')
-    print(locals())
+# def test():
+#     print('before define a')
+#     print(locals())
+#     a = 1
+#     print('after define a')
+#     print(locals())
 
-test()
+# test()
+
+# staticmethod和classmethod
+class Dog:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def run(self):
+        """
+        定义普通方法（至少需要一个 self 参数）
+        """
+        print(format(self.name), '{}会润')
+
+    @classmethod
+    def classMtthodDog(cls, x):
+        """
+        定义类方法：至少需要一个 cls 参数
+        """
+        print('Executing classDog({}, {})'.format(cls, x))
+
+    @staticmethod
+    def staticMethodDog(x):
+        """
+        定义静态方法：无默认参数
+        """
+        print('Executing staticMethodDOg({})'.format(x))
